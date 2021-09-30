8c8708fcac9b6a842b5979f5f570d4c4ef8768aa
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-09-30 21:06:09 +0900

    [feat] db에 todo list 저장/번호달기

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
 
 var db;
-MongoClient.connect('mongodb+srv://asdf1234:asdf1234@cluster0.qfyao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function(err, client) {
+MongoClient.connect('mongodb+srv://asdf1234:asdf1234@cluster0.qfyao.mongodb.net/todoapp?retryWrites=true&w=majority', function(err, client) {
 
     if(err) return console.log(err)
     db = client.db('todoapp');
 
-    db.collection('post').insertOne({이름: 'ㅁㄴㅇㄹ', 나이: 12},function(err, result) {
-        console.log('저장완료');
-    });
+    // db.collection('post').insertOne({이름: 'john', 나이: 12},function(err, result) {
+    //     console.log('저장완료');
+    // });
 
     app.listen(8080, function() {
         console.log('listening on 8080');
@@ -36,11 +36,23 @@ app.get('/write', function(req, res) {
 
 app.post('/add', function(req, res) {
     res.send('전송완료');
-    db.collection('post').insertOne({제목: req.body.title, 날짜: req.body.date}, function(err, result) {
+    db.collection('counter').findOne({name: '게시물갯수'}, function(err, result) {
+        console.log(result.totalPost);
+        var totalPostNum = result.totalPost;
+        console.log(req.body.title, req.body.date)
+
+        db.collection('post').insertOne({ _id: totalPostNum+1 , 제목: req.body.title, 날짜: req.body.date}, function(err, result) {
         console.log('저장완료');
+        db.collection('counter').updateOne({name: '게시물갯수'}, {$inc: {totalPostNum:1}}, function(err, result) {
+            if(err) {return console.log(err)}
+        })
+    });
+
     });
 });
 
 app.get('/list', function(req, res) {
-    req.prependListener('list.ejs');
+    db.collection('post').find().toArray(function(err, result){
+    res.render('list.ejs', {posts: result});
+    });
 });
