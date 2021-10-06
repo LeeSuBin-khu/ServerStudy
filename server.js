fc46ad0e119cc3c3044621740c04bdd77f05b313
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-10-06 20:47:21 +0900

    [feat] 삭제 기능

--- server.js
+++ server.js
@@ -52,3 +52,12 @@ app.get('/list', function(req, res) {
     res.render('list.ejs', {posts: result});
     });
 });
+
+app.delete('/delete', function(req, res) {
+    console.log(req.body);
+    req.body._id = parseInt(req.body._id);
+    db.collection('post').deleteOne(req.body, function(err, result){
+        console.log('삭제완료');
+        res.status(200).send({ message: '성공했습니다'});
+    })
+})
